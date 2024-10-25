4c2b633ac942cc5f11257f367e16e67297217b95
Danijela Oljaca <danijela.oljaca@nfinnova.com>
2024-10-25 17:35:12 +0200

    Vezba 16 domaci

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=10; i++) {
+            System.out.println(i * 1);
+        }
+
+        for (int i=1; i<=10; i++) {
+            System.out.println(i * 2);
+        }
+
+        for (int i=1; i<=10; i++){
+            System.out.println(i*3);
+        }
+
+    }
+}
